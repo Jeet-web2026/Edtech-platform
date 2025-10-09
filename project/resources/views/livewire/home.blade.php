f8561912be619204a0fe110a72d9566ccc7ebe0a
Jeet <jeetnath2110@gmail.com>
2025-10-09 07:08:25 +0530

    Company profile slider done

--- project/resources/views/livewire/home.blade.php
+++ project/resources/views/livewire/home.blade.php
@@ -261,6 +261,42 @@
                     }
                 ]
             });
+
+            $('.company-profile-showcase').slick({
+                dots: false,
+                infinite: true,
+                arrows: false,
+                slidesToShow: 10,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 0,
+                speed: 3000,
+                cssEase: 'linear',
+                pauseOnHover: false,
+                responsive: [{
+                        breakpoint: 1024,
+                        settings: {
+                            slidesToShow: 5,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 600,
+                        settings: {
+                            slidesToShow: 4,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 480,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1
+                        }
+                    }
+                ]
+            });
+
         });
     </script>
     @endsection
--- project/resources/views/livewire/tierupcompanies.blade.php
+++ project/resources/views/livewire/tierupcompanies.blade.php
@@ -1,6 +1,11 @@
 <div>
     <div class="py-2">
-        <div class="flex">
+        <div class="company-profile-showcase">
+            @for( $i = 15; $i >= 0; $i-- )
+            <div>
+                <img src="{{ asset('assets/images/course-img-1.jpg') }}" alt="cmpany-name" class="h-20 w-30 object-cover rounded opacity-75 hover:opacity-100 transition ease-in-out duration-300">
+            </div>
+            @endfor
         </div>
     </div>
 </div>
