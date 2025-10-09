70fe064c85af5962249ea2fdb9c26d5c4aadb7fb
Jeet <jeetnath2110@gmail.com>
2025-10-09 07:33:42 +0530

    Footer running

--- project/resources/views/livewire/footer.blade.php
+++ project/resources/views/livewire/footer.blade.php
@@ -1,12 +1,17 @@
 <div>
     <footer class="w-full bg-[#603CF6]">
-        <div class="container mx-auto py-4">
-            <div class="grid lg:grid-cols-4 md-grid-cols-3 grid-cols-1 gap-3">
-                <div>
-                    <a href="/" wire:navigate class="flex flex-row items-center gap-1">
-                        <span class="bg-[#603CF6] text-2xl shadow-sm px-3 rounded text-white">P</span>
+        <div class="container mx-auto py-8">
+            <div class="flex lg:flex-row flex-col">
+                <div class="flex flex-col gap-3 w-100">
+                    <a href="/" wire:navigate class="flex flex-row items-center gap-1 mb-3">
+                        <span class="bg-white text-2xl shadow-sm px-3 rounded text-[#603CF6]">P</span>
                         <h2 class="capitalize text-2xl font-medium text-white">pathshala</h2>
                     </a>
+                    <p class="text-base text-white font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe dignissimos sint ea quae dolore itaque perferendis, at velit asperiores nobis.</p>
+                    <div class="flex justify-start items-center gap-2 mt-3">
+                        <input type="text" class="p-2 border border-white outline-none text-white placeholder:text-white w-80" placeholder="Email address">
+                        <button class="bg-white text-[#2e1b7e] px-4 py-2 cursor-pointer font-medium border border-white">Subscribe!</button>
+                    </div>
                 </div>
             </div>
         </div>
