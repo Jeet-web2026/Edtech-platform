62504f9a7ef67f309d6dc596b759097701421622
Jeet <jeetnath2110@gmail.com>
2025-10-09 07:17:37 +0530

    Footer running

--- project/resources/views/livewire/footer.blade.php
+++ project/resources/views/livewire/footer.blade.php
@@ -1,3 +1,14 @@
 <div>
-    
+    <footer class="w-full bg-[#603CF6]">
+        <div class="container mx-auto py-4">
+            <div class="grid lg:grid-cols-4 md-grid-cols-3 grid-cols-1 gap-3">
+                <div>
+                    <a href="/" wire:navigate class="flex flex-row items-center gap-1">
+                        <span class="bg-[#603CF6] text-2xl shadow-sm px-3 rounded text-white">P</span>
+                        <h2 class="capitalize text-2xl font-medium text-white">pathshala</h2>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </footer>
 </div>
