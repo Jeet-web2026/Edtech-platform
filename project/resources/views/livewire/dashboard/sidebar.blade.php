def6c7a6caf1340e2012941d2447e516ff35971d
Jeet <jeetnath2110@gmail.com>
2025-10-14 23:12:24 +0530

    Dashboard running on Livewire and Blade

--- project/resources/views/livewire/dashboard/sidebar.blade.php
+++ project/resources/views/livewire/dashboard/sidebar.blade.php
@@ -8,22 +8,32 @@
                     Dashboard
                 </a>
             </li>
+            @if(Auth::user()->type !== 'user')
             <li class="w-full">
                 <a href="{{ route('dashboard', request()->segment(3)) }}" class="p-3 text-base font-medium bg-blue-50 text-blue-800 w-full flex flex-row items-center gap-2 border-b border-white">
                     <i class="ri-settings-line text-lg"></i>
                     Page settings
                 </a>
             </li>
+            @endif
             <li class="w-full">
                 <a href="{{ route('dashboard', request()->segment(3)) }}" class="p-3 text-base font-medium bg-blue-50 text-blue-800 w-full flex flex-row items-center gap-2 border-b border-white">
                     <i class="ri-book-marked-line text-lg"></i>
                     Courses
                 </a>
             </li>
+            @if(Auth::user()->type !== 'user')
             <li class="w-full">
                 <a href="{{ route('dashboard', request()->segment(3)) }}" class="p-3 text-base font-medium bg-blue-50 text-blue-800 w-full flex flex-row items-center gap-2 border-b border-white">
                     <i class="ri-group-line text-lg"></i>
                     Enrollments
+                </a>
+            </li>
+            @endif
+            <li class="w-full">
+                <a href="{{ route('dashboard', request()->segment(3)) }}" class="p-3 text-base font-medium bg-blue-50 text-blue-800 w-full flex flex-row items-center gap-2 border-b border-white">
+                    <i class="ri-exchange-line text-lg"></i>
+                    Fees update
                 </a>
             </li>
         </ul>
