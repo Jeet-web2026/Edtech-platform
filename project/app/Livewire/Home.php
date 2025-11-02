85b7ce9c76b13fffb402a16e977c948e6d82ce76
Jeet <jeetnath2110@gmail.com>
2025-11-02 13:50:46 +0530

    Initial changes on dynamic fetching of our partners in home component.

--- project/app/Livewire/Home.php
+++ project/app/Livewire/Home.php
@@ -11,6 +11,7 @@ class Home extends Component
     {
         $ourPartners = User::with(['adminDetails:id,user_id,profile'])
             ->where('role', 'admin')
+            ->where('is_active', 1)
             ->select('id', 'first_name', 'last_name')
             ->get();
 
